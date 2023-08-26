9a7db6468516bad416aa90d2eada9f1c53e4cf5d
atik <atikur2013rahman@gmail.com>
2023-08-26 11:16:21 +0600

    ejs

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+
+app.set("view engine", "ejs");
+
+let pLanguages = [];
+
+app.get("/", (req, res) => {
+  res.render("index.ejs", { plNames: pLanguages });
+});
+
+app.post("/", (req, res) => {
+  const pLanguage = req.body.pLanguage;
+  pLanguages.push(pLanguage);
+  res.redirect("/");
+});
+
+app.listen(port, () => {
+  console.log(`The server is running at http://localhost:${port}`);
+});
