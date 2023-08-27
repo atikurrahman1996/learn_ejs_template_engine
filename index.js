b8f3b0d80cf091b8ae9c5a8ba250e9042c9cb944
atik <atikur2013rahman@gmail.com>
2023-08-27 10:05:57 +0600

    app using ejs

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+//for heroku deploy
+//const port = process.env.PORT;
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
